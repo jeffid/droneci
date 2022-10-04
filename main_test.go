2e607fd90114bd625468c2e931b9e9e698446cb6
Jeff <jeffid@qq.com>
2022-10-04 22:59:48 +0800

    1.0

--- main_test.go
+++ main_test.go
@@ -1,8 +1,25 @@
 package main
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v9"
+	"testing"
+)
 
 func TestEcho(t *testing.T) {
 	Echo()
 	t.Log("test OK")
+}
+
+func TestRedis(t *testing.T) {
+	ctx:=context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	res:=rdb.Incr(ctx,"after")
+	fmt.Println("redis res: ",*res)
 }
